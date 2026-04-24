6b35bbc083affd5b09108866b751249b43ff86ed
@akhavr <akhavr@42cc.co>
2026-04-24 16:47:52 +0000

    wip: max-turns at step 0 [f81c2aed2986]

--- tests/watcher/test_session_monitor.py
+++ tests/watcher/test_session_monitor.py
@@ -1358,6 +1358,8 @@ class TestStaleReviewSessionCleanup:
         # Coder should be relaunched
         assert "abc" in launched
 
+=======
+>>>>>>> 06a74629 (wip: resolve bcff6564f9ee at step 0 [bcff6564f9ee])
 
 # ---------------------------------------------------------------------------
 # Orphan with @@DONE@@ marker tests
